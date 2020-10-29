ff341f363d6509f8352d35f5d46054447f49d21c
alexn34-n <alexn3469@mail.ru>
2020-10-29 19:24:57 +0300

    Выполненная первая домашняя работа

---
+++ src/ru/alexn/gb/HomeTask5Lesson1.java
@@ -0,0 +1,14 @@
+package ru.alexn.gb;
+//Created by Alexey Nikolaev
+
+public class HomeTask5Lesson1 {
+    public static void main(String[] args) {
+        int a=-14;
+        if (a>=0) {
+            System.out.println("Число является положительным и равно"+a);
+        }else {
+            System.out.println("Число является отрицательным и  равно"+a);
+        }
+    }
+
+}
