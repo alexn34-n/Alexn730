5144e390df7325efbedc17c4600d20915853c954
alexn34-n <alexn3469@mail.ru>
2020-11-21 14:02:42 +0300

    Выполнил домашнюю работу к пятому уроку

---
+++ src/ru/alexn/gb/MyWindow.java
@@ -0,0 +1,49 @@
+package ru.alexn.gb;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindow extends JFrame
+{
+    MyWindow()
+    {
+        setVisible(true);
+
+        setLocation(350,300);
+        setSize(350,300);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("My Window");
+
+        JPanel panel= new JPanel(new GridLayout(1,2));
+
+        JButton btn1= new JButton("Выход");
+        btn1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        });
+        panel.add(btn1);
+
+        JButton btn2= new JButton("Новое окно");
+        btn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                MyWindow w= new MyWindow();
+            }
+        });
+
+        panel.add(btn2);
+
+        add(panel,BorderLayout.SOUTH);
+
+
+
+    }
+
+}
